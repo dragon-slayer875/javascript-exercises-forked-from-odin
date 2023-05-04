42258a696ed38f4221c3ae164a5b9abc838ed3d0
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-05-04 09:38:05 +0530

    Complete exercsie 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	// let result = 0;
+  // numArray.forEach(element => {
+  //   result += element;
+  // });
+  // return result;
+  // Logic is correct but the exercises intent was to learn array methods.
+
+  return numArray.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function(numArray) {
+  // let result = 1;
+  // numArray.forEach(element => {
+  //   result *= element;
+  //   return result
+  // });
+  // return result;
+  // Logic is correct but the programs intent was to learn array methods.
 
+  return numArray.length
+    ? numArray.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  if (num == 0) {
+    return result;
+  }
+  else {
+    for (let i = num; i > 0; i--) {
+      result *= i;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
