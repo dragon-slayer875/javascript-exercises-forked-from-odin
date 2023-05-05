fdca97c19a64b17cf2a50b3b7ab809f53be4d999
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2023-05-05 16:36:23 +0530

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const todaysDate = new Date().getFullYear()
+    people.sort((person1, person2) => 
+    {return (((person2.yearOfDeath? person2.yearOfDeath : todaysDate) - person2.yearOfBirth) 
+        - ((person1.yearOfDeath? person1.yearOfDeath : todaysDate) - person1.yearOfBirth));})
+    return people[0]
 };
 
 // Do not edit below this line
